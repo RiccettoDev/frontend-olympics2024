10875e30065d769669b6fd9a19e9a61d555f77cb
Eduardo <princesherlockk@gmail.com>
2024-03-16 15:28:35 -0300

    [frontend-olympics2024]-feat- Create pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,13 @@
 export default function About(){
-  return <h1>Sobre</h1>
+  return(
+    <div className="w-full min-h-screen bg-[#c0c0c0] flex justify-center px-4">
+      <main className="my-10 w-full">
+
+        <div className='text-center mb-6'>
+          <h1 className="text-[48px] font-medium text-indigo-800">Sobre</h1>
+        </div>
+
+      </main>
+    </div>
+  )
 }
--- src/pages/Stadiums.tsx
+++ src/pages/Stadiums.tsx
@@ -1,3 +1,13 @@
 export default function Stadiums(){
-  return <h1>Arenas</h1>
+  return(
+    <div className="w-full min-h-screen bg-[#c0c0c0] flex justify-center px-4">
+      <main className="my-10 w-full">
+
+        <div className='text-center mb-6'>
+          <h1 className="text-[48px] font-medium text-indigo-800">Estadios</h1>
+        </div>
+
+      </main>
+    </div>
+  )
 }
