4ce9d13310672fdd46f9c53c9dbc3e1b763f43ea
Eduardo <princesherlockk@gmail.com>
2024-03-18 17:54:36 -0300

    [frontend-olympics2024]-fix- Adjust Layout responsive

--- src/components/CardAbout.tsx
+++ src/components/CardAbout.tsx
@@ -4,22 +4,26 @@ interface CardAboutProps{
   text1: string
   text2: string
   text3: string
+  text4: string
 }
 
-export default function CardAbout({title, img, text1, text2, text3}: CardAboutProps){
+export default function CardAbout({title, img, text1, text2, text3, text4}: CardAboutProps){
   return(
-    <div className="flex flex-col w-[50%] justify-center items-center m-4 bg-indigo-800 p-8 rounded-md shadow-2xl shadow-black">
+    <div className="flex flex-col w-[90%] justify-center items-center m-4 bg-indigo-800 p-8 rounded-md shadow-2xl shadow-black">
       <div className='flex flex-col justify-center items-center rounded bg-indigo-500 p-8 shadow-2xl shadow-black'>
-        <h2 className='text-[24px] font-extrabold text-white mb-4'>{title}</h2>
+        <h2 className='text-[20px] font-extrabold text-white mb-4 md:text-[24px]'>{title}</h2>
         <img src={img} alt="Imagem Olimpiadas 2024" className='rounded w-[85%] mb-6 shadow-2xl shadow-black'/>
-        <h3 className='text-[18px] font-extrabold text-white p-4'>
+        <h3 className='text-[16px] font-extrabold text-white mb-4 md:text-[22px]'>
           {text1}
         </h3>
-        <h3 className='text-[18px] font-extrabold text-white p-4'>
+        <h3 className='text-[16px] font-extrabold text-white mb-4 md:text-[22px]'>
           {text2}
         </h3>
-        <h3 className='text-[18px] font-extrabold text-white p-4'>
+        <h3 className='text-[16px] font-extrabold text-white mb-4 md:text-[22px]'>
           {text3}
+        </h3>
+        <h3 className='text-[16px] font-extrabold text-white mb-4 md:text-[22px]'>
+          {text4}
         </h3>
       </div>
     </div>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -10,20 +10,22 @@ export default function About(){
 
         <div className='text-center mb-6'>
           <h1 className="text-[48px] font-extrabold text-indigo-800">Sobre</h1>
-          <div className="flex">
+          <div className="flex flex-col lg:flex-row justify-center items-center">
             <CardAbout 
               title='Conheça algumas curiosidades sobre as Olimpíadas:'
               img={olympics}
               text1='As Olimpíadas na Antiguidade surgiram por volta de 776 a. C., conforme registros históricos. Entretanto, não há consenso entre os pesquisadores sobre um fato exato que tenha marcado o início dos jogos. Na Antiguidade, os jogos estavam associados a rituais religiosos. Nesse sentido, os gregos homenageavam Zeus, rei dos deuses na mitologia grega.'
               text2='Os jogos recebem esse nome, pois começaram na cidade grega de Olímpia, situada no sudoeste da Grécia. As competições aconteciam nos momentos de trégua, pois naquela época eram comuns conflitos entre as cidades-estado gregas. A situação de trégua era decretada dois meses antes dos jogos, que ocorriam também de quatro em quatro anos, só que sempre em uma mesma cidade, Olímpia.'
               text3='O anúncio do evento era dado por mensageiros em diferentes regiões, para que as pessoas pudessem viajar para Olímpia em segurança, já que os conflitos estariam interrompidos durante a competição.' 
+              text4=''
             />
             <CardAbout
               title='Curiosidades sobre a próxima Olimpíada:'
               img={paris2024}
-              text1='Com os jogos de 2024, Paris terá sido sede das olímpiadas por três vezes (1900, 1994 e 2024), a única com o feito ao lado de Londres (1908, 1948, 2012). Tornando a França o segundo país que mais sediou os jogos olimpicos, ficando atrás somento dos EUA.'
+              text1='Com os jogos de 2024, Paris terá sido sede das olímpiadas por três vezes (1900, 1994 e 2024), a única com o feito ao lado de Londres (1908, 1948, 2012).'
               text2='Pela primeira vez na história, a cerimônia de abertura dos jogos não será feita em um estádio e sim no rio Sena, que cruza a cidade de país.'
               text3='Desde 2016, pessoas refugiadas integram uma nova equipe de competição em que não defendem nenhum país específico e sim como "equipe olímpica de refugiados" e também estarão presentes em 2024.'
+              text4='Phryges foi escolhida como mascote dos Jogos Olímpicos de Paris. A inspiração são os famosos barrete frígio, chapéus associados a ideia de liberdade usado por franceses na época da Revolução.'
             />
           </div>
         </div>
