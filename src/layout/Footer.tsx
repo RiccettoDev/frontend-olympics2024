92dfa150c6201f90300251ae0d29ee679c6060e8
Eduardo <princesherlockk@gmail.com>
2024-03-19 17:13:33 -0300

    [frontend-olympics2024]-feat- Create and implement Footer in Layout

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+  return(
+    <div className="flex justify-center items-center bg-indigo-800 h-10">
+      <a href="https://portfolio-ten-lime-67.vercel.app/" target="_blank">
+        <h2 className="text-white font-extrabold"> &copy; 2004 - Euardo Riccetto</h2>
+      </a>
+    </div>
+  )
+}
